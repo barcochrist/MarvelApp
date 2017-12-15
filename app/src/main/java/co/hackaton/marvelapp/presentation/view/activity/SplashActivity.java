827cac0ea6e764ee338437a83da5db3d9314a529
barcochrist <barcochrist@gmail.com>
2017-12-14 21:02:03 -0500

    Creación de diseño de Splash
    - Se declara un tiempo de espera para el Splash
    - Se implementa que se muestre en pantalla completa
    - Refactorización en la ubicación del SplashActivity

---
+++ app/src/main/java/co/hackaton/marvelapp/presentation/view/activity/SplashActivity.java
@@ -0,0 +1,37 @@
+package co.hackaton.marvelapp.presentation.view.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import co.hackaton.marvelapp.MainActivity;
+import co.hackaton.marvelapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
