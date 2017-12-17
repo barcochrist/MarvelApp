9e9dad0a92ba844532cbc459b5ed87b42f066905
barcochrist <barcochrist@gmail.com>
2017-12-16 19:51:00 -0500

    Refactorización de MainActivity
    Creación de actividad basada en plantillas de Android Studio
    Creación de fragmento inicial

--- app/src/main/java/co/hackaton/marvelapp/presentation/view/activity/SplashActivity.java
+++ app/src/main/java/co/hackaton/marvelapp/presentation/view/activity/SplashActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import co.hackaton.marvelapp.MainActivity;
 import co.hackaton.marvelapp.R;
 
 public class SplashActivity extends AppCompatActivity {
