00fc8f53bbd725e7a6a8c40b4a5b8e98aacbcf95
barcochrist <barcochrist@gmail.com>
2017-12-25 11:14:28 -0500

    Creación constantes para identificar variables en Actividades
    Refactorización de interface a clase
    Creación de propiedades estáticas

--- app/src/main/java/co/hackaton/marvelapp/helpers/Constants.java
+++ app/src/main/java/co/hackaton/marvelapp/helpers/Constants.java
@@ -4,13 +4,23 @@ package co.hackaton.marvelapp.helpers;
  * Created by casa on 16/12/17.
  */
 
-public interface Constants {
+public final class Constants {
 
     public static final String API_KEY = "091cea49aac355a7382573d88dfda6bc";
     public static final String HASH = "63cc729ba71911208b0c7af3f3e0fd70";
 
     /**
-     * Keys para identificar los nodos principales en la Base de Datos de firebase
+     * Llaves para identificar los nodos principales en la Base de Datos de firebase
      */
     public static final String CHARACTERS_KEY_FIREBASE = "characters";
+    public static final String COMICS_KEY_FIREBASE = "comics";
+    public static final String SERIES_KEY_FIREBASE = "series";
+
+    /**
+     * Llaves para identificar a cada uno de los fragmentos.
+     * Util para el paso de variables entre actividades o fragmentos.
+     */
+    public static final String CHARACTER_KEY = "CHARACTER";
+    public static final String COMIC_KEY = "COMICS";
+    public static final String SERIE_KEY = "SERIES";
 }
